b2095e6cc10498ad10a48482616ff74bed1de1b7
Edmilson Soares <esbnet@gmail.com>
2022-08-24 20:50:06 -0300

    refactor: :recycle: create avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import styles from "./Sidebar.module.css";
 
+import {Avatar} from './Avatar.jsx'
+
 import { PencilLine } from 'phosphor-react'
 export function Sidebar() {
     return (
@@ -9,7 +11,7 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1555952494-efd681c7e3f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8OTc2ODY3NHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="cover from profile"
             />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/esbnet.png" alt="avatar" />
+                <Avatar/>
                 <strong>Edmilson Soares</strong>
                 <span>Full Stack Developer</span>
             </div>
