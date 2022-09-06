8d6f09a68ceadefee7994ac3ac6f1895a5ebf7a5
Edmilson Soares <esbnet@gmail.com>
2022-09-06 18:04:58 -0300

    feat: :sparkles: add likes on comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
@@ -5,8 +7,14 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 
 export function Comment({ content, onDeleteComment }) {
 
+    const [likeCount, setLikeCount] = useState(0);
+
     function handleDeleteComment(){
         onDeleteComment(content);
+    }
+
+    function handleAddLike() {
+        setLikeCount(likeCount + 1);
     }
 
     return (
@@ -26,9 +34,9 @@ export function Comment({ content, onDeleteComment }) {
                     <p> {content}</p>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={handleAddLike}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
